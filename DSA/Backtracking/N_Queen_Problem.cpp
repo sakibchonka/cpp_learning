b9f2300aa36c802ae40534e88c302362a3e8e345
sakib <sakibchonka@gmail.com>
2021-08-02 20:19:32 +0530

    02/08/2021

---
+++ DSA/Backtracking/N_Queen_Problem.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define N 4
+int board[N][N];
+
+void printSolution(int board[N][N]){
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            cout<<board[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
+}
+
+bool isSafe(int row,int col){
+    for(int i=0;i<col;i++)
+        if(board[row][i])
+            return false;
+    for(int i=row,j=col;i>=0 && j>=0;i--,j--)
+        if(board[i][j])
+            return false;
+    for(int i=row,j=col;j>=0 && i<N;i++,j--)
+        if(board[i][j])
+            return false;
+    return true;
+}
+
+bool solveRec(int col){
+    if(col==N) return true;
+    for(int i=0;i<N;i++){
+        if(isSafe(i,col)){
+            board[i][col]=1;
+            if(solveRec(col+1))
+                return true;
+            board[i][col]=0;
+        }
+    }
+    return false;
+}
+
+void solve(){
+    if(solveRec(0)==false){
+        cout<<"Solution does not exist";
+        return;
+    }
+    printSolution(board);
+    return;
+}
+
+int main(){
+    solve();
+
+    return 0;
+}

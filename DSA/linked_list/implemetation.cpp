983ba05c4cbedb9e157a39ddce0a61c1cf2cc825
sakib <sakibchonka@gmail.com>
2021-08-15 23:33:44 +0530

    15/08/2021

--- DSA/linked_list/implemetation.cpp
+++ DSA/linked_list/implemetation.cpp
@@ -28,18 +28,63 @@ Node* insertBegin(Node *head,int x){
     return temp;
 }
 
+Node* delHead(Node *head){
+    if(head == nullptr)
+        return nullptr;
+    else{
+        Node *temp = head->next;
+        delete head;
+        return temp;
+    }
+}
+
+Node* delTail(Node *head){
+    if(head == nullptr)
+        return nullptr;
+    if(head->next == nullptr){
+        delete head;
+        return nullptr;
+    }
+    Node *curr = head;
+    while(curr->next->next !=nullptr){
+        curr = curr->next;
+    }
+    delete curr->next;
+    curr->next =nullptr;
+    return head;
+}
+
+Node* insertPos(Node *head,int pos,int data){
+    Node *temp = new Node(data);
+    if(pos == 1){
+        temp->next = head;
+        return temp;
+    }
+    Node *curr = head;
+    for(int i=1; i<=pos-2 && curr!=nullptr; i++){
+        curr = curr->next;
+    }
+    if(curr == nullptr) 
+        return head;
+    temp->next = curr->next;
+    curr->next = temp;
+    return head;
+}
+
 void printlist(Node *head){
     while(head!=nullptr){
         cout<<head->data<<" ";
         head = head->next;
     }
+    cout<<"\n";
 }
 
 int main(){
-    Node *head=NULL;
-	head=insertEnd(head,10);
-	head=insertEnd(head,20);
-	head=insertEnd(head,30);
+    Node *head=new Node(10);
+	head->next=new Node(20);
+	head->next->next=new Node(30);
+	printlist(head);
+	head=delTail(head);
 	printlist(head);
 
     return 0;
